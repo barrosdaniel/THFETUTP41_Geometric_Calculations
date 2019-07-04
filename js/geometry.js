00e4e128abca0381527c1fd86e8dfb3838d3cde3
Daniel Barros <barros.daniel@outlook.com>
2019-07-04 12:35:12 +0800

    feature: Add circle area calculation function

--- js/geometry.js
+++ js/geometry.js
@@ -1,11 +1,29 @@
+// Rectangle Area
 function calculateRectangleArea(width, height) {
   return width * height;
 }
 
 console.log(calculateRectangleArea(10, 20));
 
+// Prism Volume
 function calculatePrismVolume(width, height, length) {
   return width * height * length;
 }
 
 console.log(calculatePrismVolume(10, 20, 15));
+
+// Circle Area
+function calculateCircleArea(radius) {
+
+  return Math.PI * Math.pow(radius, 2);
+}
+
+console.log(calculateCircleArea(2));
+
+// Sphere Volume
+function calculateSphereVolume(radius) {
+
+  return Math.PI * Math.pow(radius, 2);
+}
+
+console.log(calculateCircleArea(2));
